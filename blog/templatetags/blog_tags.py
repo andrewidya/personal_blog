3bbfc0fb56832badffadc136dcaefa78c7b25e94
Andry UN <andrywidyaputra@gmail.com>
2018-11-28 23:04:12 +0700

    Fix: has_recent_posts, get_popular_posts, get_recent_posts function
    file:
        blog/templatetags/blog_tags.py

--- blog/templatetags/blog_tags.py
+++ blog/templatetags/blog_tags.py
@@ -1,25 +1,39 @@
 from django import template
+from django.db.models import Count, Q
+
+from blog.models import BlogPage, PageHitCounter
 
 
 register = template.Library()
 
-@register.simple_tag
+@register.simple_tag()
+def has_recent_posts():
+    pages = BlogPage.objects.live().order_by('date_published')
+    return pages.exists()
+
+@register.inclusion_tag("blog/tags/recent_posts.html", takes_context=False)
 def get_recent_posts(items=0):
-    from blog.models import BlogPage
+    pages = BlogPage.objects.live().order_by('date_published')
     if items > 0:
-        return BlogPage.objects.live()[:items]
-    return BlogPage.objects.live()[:5]
+        pages = pages[:items]
+    else:
+        pages = pages[:4]
 
+    return {
+        'pages': pages
+    }
 
-@register.simple_tag
+@register.inclusion_tag("blog/tags/popular_posts.html", takes_context=False)
 def get_popular_posts(max=10):
-    from django.db.models import Count, Q
-    from blog.models import PageHitCounter, BlogPage
-    
     hit_list = PageHitCounter.objects.values('page').annotate(Count('page')).order_by('-page__count')[:max]
     page_id = [ x['page'] for x in hit_list ]
+
     filters = Q(pk=page_id[0])
     for x in page_id[1:]:
         filters |= Q(pk=x)
+
     posts = BlogPage.objects.filter(filters)
-    return posts
+
+    return {
+        'pages' : posts
+    }
